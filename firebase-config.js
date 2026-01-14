b90fafb308ac5b99fd7ead1b9a2a64df3944689f
SubversionGames <subversionmole@gmail.com>
2026-01-14 01:01:43 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,24 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  databaseURL: "https://your-project.firebaseio.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "YOUR_APP_ID"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+// Game passwords
+const PASSWORDS = {
+  host: "SVTr3",      // Change this to your host password
+  player: "Player2026"     // Change this to your player password
+};
+
+// Export for use in other files
+window.gameDatabase = database;
+window.gamePasswords = PASSWORDS;
